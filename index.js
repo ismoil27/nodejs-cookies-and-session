deb6f88e53ead43039034a04d7d57e803b5e2dde
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-08-18 17:44:05 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const mysql = require("mysql");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const db = require("./config/db");
+const PORT = process.env.PORT || 3000;
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.post("/register", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+    }
+
+    db.query(
+      "insert into users (name, email, password) values (?, ?, ?)",
+      [name, email, hash],
+      (err, result) => {
+        console.log(err);
+      }
+    );
+  });
+  res.send({ message: req.body });
+});
+
+app.post("/login", (req, res) => {
+  // const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query(
+    "select * from users where email = ?;",
+
+    email,
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+      }
+      if (result.length > 0) {
+        bcrypt.compare(password, result[0].password, (err, response) => {
+          if (response) {
+            res.send(result);
+          } else {
+            res.send({ message: `Wrong credentials` });
+          }
+        });
+      } else {
+        res.send({ message: `User does not exist` });
+      }
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
